7b7dd818b30ff184d1205d12957beb7991a0337d
Hecate2 <2474101468@qq.com>
2024-01-08 13:08:49 +0800

    use table name instead of id

--- RelationalDB.cs
+++ RelationalDB.cs
@@ -25,9 +25,17 @@ namespace SecondaryIndex
         //const byte TYPE_STRUCT = 0x41;
         //const byte TYPE_MAP = 0x48;
 
-        const byte USER_TO_TABLE_ID_PREFIX = (byte)'U';  // 0x55
-        const byte USER_TABLE_ID_TO_COLUMNS_PREFIX = (byte)'T';  // 0x54
-        const byte DROPPED_TABLE_ID_TO_COLUMNS_PREFIX = (byte)'D';  // 0x44
+        const byte USER_TABLE_NAME_TO_COLUMNS_PREFIX = (byte)'T';  // 0x54     user + tableName -> columnTypes
+        const byte DROPPED_TABLE_NAME_TO_COLUMNS_PREFIX = (byte)'D';  // 0x44  user + tableName -> columnTypes
+
+        [Safe]
+        public static Iterator ListTables(UInt160 user, ByteString tableNamePrefix) => new StorageMap(USER_TABLE_NAME_TO_COLUMNS_PREFIX).Find(user + tableNamePrefix);
+        [Safe]
+        public static Iterator ListAllTables() => new StorageMap(USER_TABLE_NAME_TO_COLUMNS_PREFIX).Find();
+        [Safe]
+        public static Iterator ListDroppedTables(UInt160 user, ByteString tableNamePrefix) => new StorageMap(DROPPED_TABLE_NAME_TO_COLUMNS_PREFIX).Find(user + tableNamePrefix);
+        [Safe]
+        public static Iterator ListAllDroppedTables() => new StorageMap(DROPPED_TABLE_NAME_TO_COLUMNS_PREFIX).Find();
 
         /// <summary>
         /// 
@@ -37,7 +45,7 @@ namespace SecondaryIndex
         /// <returns></returns>
         /// <exception cref="ArgumentOutOfRangeException"></exception>
         /// <exception cref="ArgumentException"></exception>
-        public static BigInteger CreateTable(UInt160 user, ByteString columnTypes)
+        public static void CreateTable(UInt160 user, ByteString tableName, ByteString columnTypes)
         {
             ExecutionEngine.Assert(Runtime.CheckWitness(user), "witness CreateTable");
             if (columnTypes.Length > 256) throw new ArgumentOutOfRangeException("Too many columns");
@@ -46,37 +54,41 @@ namespace SecondaryIndex
             for (int i = 0; i < l; ++i)
             {
                 byte type = columnTypes[i];
-                if (type == TYPE_BOOLEAN || type != TYPE_INTEGER_VAR_LENGTH || type == TYPE_BYTESTRING_VAR_LENGTH)
+                if (type == TYPE_BOOLEAN || type == TYPE_INTEGER_VAR_LENGTH || type == TYPE_BYTESTRING_VAR_LENGTH)
                     continue;
-                if (type == TYPE_INTEGER_FIXED_LENGTH || type != TYPE_BYTESTRING_FIXED_LENGTH)
+                if (type == TYPE_INTEGER_FIXED_LENGTH || type == TYPE_BYTESTRING_FIXED_LENGTH)
                 {
                     ++i;
                     // now columnTypes[i] refers to the (fixed) length of the value, in count of bytes
+                    if (columnTypes[i] == 0x00)
+                        throw new ArgumentException("Invalid length 0x00");
                     continue;
                 }
                 throw new ArgumentException("Invalid type " + type);
             }
 
             StorageContext context = Storage.CurrentContext;
-            StorageMap userToTableId = new(context, USER_TO_TABLE_ID_PREFIX);
-            ByteString byteStringId = userToTableId.Get(user);
-            BigInteger bigIntegerId = (BigInteger)byteStringId;
-            userToTableId.Put(user, bigIntegerId + 1);
-            new StorageMap(context, USER_TABLE_ID_TO_COLUMNS_PREFIX).Put(user + byteStringId, columnTypes);
-            return bigIntegerId;
+            ByteString key = user + tableName;
+            ByteString droppedTable = new StorageMap(context, DROPPED_TABLE_NAME_TO_COLUMNS_PREFIX).Get(key);
+            if (droppedTable != null)
+                throw new ArgumentException("Table already dropped");
+            StorageMap createdTable = new StorageMap(context, USER_TABLE_NAME_TO_COLUMNS_PREFIX);
+            if (createdTable.Get(key) != null)
+                throw new ArgumentException("Table already created");
+            createdTable.Put(key, columnTypes);
         }
 
-        public static void DropTable(UInt160 user, ByteString tableId)
+        public static void DropTable(UInt160 user, ByteString tableName)
         {
             ExecutionEngine.Assert(Runtime.CheckWitness(user), "witness DropTable");
             StorageContext context = Storage.CurrentContext;
-            ByteString key = user + tableId;
-            StorageMap userToTableId = new StorageMap(context, USER_TABLE_ID_TO_COLUMNS_PREFIX);
-            ByteString columnTypes = userToTableId.Get(key);
+            ByteString key = user + tableName;
+            StorageMap tableToColumn = new StorageMap(context, USER_TABLE_NAME_TO_COLUMNS_PREFIX);
+            ByteString columnTypes = tableToColumn.Get(key);
             if (columnTypes == null || columnTypes.Length == 0)
-                throw new ArgumentException("No table at id " + tableId);
-            userToTableId.Delete(key);
-            new StorageMap(context, DROPPED_TABLE_ID_TO_COLUMNS_PREFIX).Put(key, columnTypes);
+                throw new ArgumentException("No table at id " + tableName);
+            tableToColumn.Delete(key);
+            new StorageMap(context, DROPPED_TABLE_NAME_TO_COLUMNS_PREFIX).Put(key, columnTypes);
         }
 
         public static ByteString EncodeInteger(BigInteger i) => EncodeByteString((ByteString)i);
--- test.py
+++ test.py
@@ -1,8 +1,35 @@
-from neo_fairy_client import FairyClient
+from neo_fairy_client import FairyClient, Hash160Str
+from enum import Enum
 
-c = FairyClient(fairy_session='RelationalDB')
+user = Hash160Str('0xb1983fa2479a0c8e2beae032d2df564b5451b7a5')
+c = FairyClient(fairy_session='RelationalDB', wallet_address_or_scripthash=user, with_print=False)
 c.virutal_deploy_from_path('./bin/sc/RelationalDB.nef')
 print(result := c.invokefunction('encodeInteger', [12345678]))
 assert c.invokefunction('decodeInteger', [result]) == (12345678, '')
 print(result := c.invokefunction('encodeInteger', [-12345678]))
 assert c.invokefunction('decodeInteger', [result]) == (-12345678, '')
+
+
+class Types(bytes, Enum):
+    Boolean = b'\x20'
+    IntegerVarLen = b'\x21'
+    ByteStringVarLen = b'\x28'
+    IntegerFixedLen = b'\x31'
+    ByteStringFixedLen = b'\x38'
+    
+    def __add__(self, other):
+        return self.value + other.value
+
+
+table_name = 'testTable'
+assert 'Invalid type' in c.invokefunction('createTable', [user, table_name, Types.Boolean + Types.IntegerVarLen + b'\x00'], do_not_raise_on_result=True)
+c.invokefunction('createTable', [user, 'testTable', Types.Boolean + Types.IntegerVarLen + Types.ByteStringVarLen + Types.IntegerFixedLen + b'\x04' + Types.ByteStringFixedLen + b'\x20'])
+assert len(c.invokefunction('listTables', [user, 'test'])) == 1
+assert len(c.invokefunction('listAllTables')) == 1
+assert len(c.invokefunction('listDroppedTables', [user, ''])) == 0
+assert len(c.invokefunction('listAllDroppedTables')) == 0
+c.invokefunction('dropTable', [user, table_name])
+assert len(c.invokefunction('listTables', [user, ''])) == 0
+assert len(c.invokefunction('listAllTables')) == 0
+assert len(c.invokefunction('listDroppedTables', [user, ''])) == 1
+assert len(c.invokefunction('listAllDroppedTables')) == 1
