b35297ae04a4e58259b553ec740ecd174964b160
Hecate2 <2474101468@qq.com>
2024-01-08 17:57:19 +0800

    allow custom primary key

--- RelationalDB.cs
+++ RelationalDB.cs
@@ -7,12 +7,12 @@ using Neo.SmartContract.Framework.Attributes;
 using Neo.SmartContract.Framework.Native;
 using Neo.SmartContract.Framework.Services;
 
-namespace SecondaryIndex
+namespace RelationalDB
 {
     [DisplayName("RelationalDB")]
     [ManifestExtra("Author", "Hecate2")]
     [ManifestExtra("Email", "developer@neo.org")]
-    [ManifestExtra("Description", "This is a SecondaryIndex")]
+    [ManifestExtra("Description", "This is a RelationalDB")]
     public class RelationalDB : SmartContract
     {
         const int LengthOfLength = 8;
@@ -22,7 +22,7 @@ namespace SecondaryIndex
         const byte BOOLEAN = 0x20;
         const byte INT_VAR_LEN = 0x21;
         const byte BYTESTRING_VAR_LEN = 0x28;
-        const byte INT_FIXED_LEN = 0x31;
+        const byte UINT_FIXED_LEN = 0x31;  // dangerous to use minus numbers with fixed length integers!
         const byte BYTESTRING_FIXED_LEN = 0x38;
         //const byte TYPE_ARRAY = 0x40;
         //const byte TYPE_STRUCT = 0x41;
@@ -31,7 +31,7 @@ namespace SecondaryIndex
 
         const byte USER_TABLE_NAME_TO_COLUMNS_PREFIX = (byte)'T';  // 0x54     user + tableName -> columnTypes
         const byte DROPPED_TABLE_NAME_TO_COLUMNS_PREFIX = (byte)'D';  // 0x44  user + tableName -> columnTypes
-        const byte SEPARATOR = (byte)'\xb6';  // ¶
+        const string SEPARATOR = "\xb6";  // ¶
         const byte ROWS_PREFIX = (byte)'R';  // 0x52  user + tableName + SEPARATOR + rowId -> data[]
         const byte TABLE_ROW_ID_PREFIX = (byte)'I';  // 0x49  user + tableName -> rowId: int
 
@@ -45,7 +45,10 @@ namespace SecondaryIndex
         public static Iterator ListAllDroppedTables() => new StorageMap(DROPPED_TABLE_NAME_TO_COLUMNS_PREFIX).Find();
 
         /// <summary>
-        /// 
+        /// Be aware that Neo3 allows only storage key no more than 64 bytes
+        /// <see cref="user"/> costs 20 bytes; a separator costs 1 byte;
+        /// you need the remaining 43 bytes for table name and primary key
+        /// DO NOT USE VERY LONG TABLE NAME
         /// </summary>
         /// <param name="user"></param>
         /// <param name="columnTypes"></param>
@@ -60,7 +63,7 @@ namespace SecondaryIndex
         public static void CreateTable(UInt160 user, ByteString tableName, ByteString columnTypes, bool useAutoIncPriKey)
         {
             ExecutionEngine.Assert(Runtime.CheckWitness(user), "witness CreateTable");
-            ExecutionEngine.Assert(StdLib.MemorySearch(tableName, (ByteString)new byte[] { SEPARATOR }) == -1,
+            ExecutionEngine.Assert(StdLib.MemorySearch(tableName, SEPARATOR) == -1,
                 "SEPARATOR in tableName");
             if (columnTypes.Length > 256) throw new ArgumentOutOfRangeException("Too many columns");
             if (columnTypes.Length == 0) throw new ArgumentException("No column specified");
@@ -71,7 +74,7 @@ namespace SecondaryIndex
                 if (type == BOOLEAN || type == INT_VAR_LEN || type == BYTESTRING_VAR_LEN || 
                     type == UINT160 || type == UINT256)
                     continue;
-                if (type == INT_FIXED_LEN || type == BYTESTRING_FIXED_LEN)
+                if (type == UINT_FIXED_LEN || type == BYTESTRING_FIXED_LEN)
                 {
                     ++i;
                     // now columnTypes[i] refers to the (fixed) length of the value, in count of bytes
@@ -123,7 +126,7 @@ namespace SecondaryIndex
                     return EncodeByteStringFixedLength((UInt160)data, 20);
                 case UINT256:
                     return EncodeByteStringFixedLength((UInt256)data, 32);
-                case INT_FIXED_LEN:
+                case UINT_FIXED_LEN:
                 case BYTESTRING_FIXED_LEN:
                     return EncodeByteStringFixedLength((ByteString)data, length);
                 default:
@@ -140,7 +143,7 @@ namespace SecondaryIndex
         /// <exception cref="ArgumentException"></exception>
         public static void WriteRow(UInt160 user, ByteString tableName, object[] row)
         {
-            ExecutionEngine.Assert(Runtime.CheckWitness(user), "witness AddRow");
+            ExecutionEngine.Assert(Runtime.CheckWitness(user), "witness WriteRow");
             ExecutionEngine.Assert(row.Length > 0, "No row");
             StorageContext context = Storage.CurrentContext;
             ByteString key = user + tableName;
@@ -158,7 +161,7 @@ namespace SecondaryIndex
             if (rowId == null)
             {
                 byte type = columnTypes[columnTypesIndexer++];
-                if (type == INT_FIXED_LEN || type == BYTESTRING_FIXED_LEN)
+                if (type == UINT_FIXED_LEN || type == BYTESTRING_FIXED_LEN)
                     primaryKey = EncodeSingle(row[rowIndexer++], type, columnTypes[columnTypesIndexer++]);
                 else
                     primaryKey = EncodeSingle(row[rowIndexer++], type, 0);
@@ -171,7 +174,7 @@ namespace SecondaryIndex
             {
                 ExecutionEngine.Assert(rowIndexer < rowLength, "Wrong count of columns: rowIndexer=" + rowIndexer);
                 byte type = columnTypes[columnTypesIndexer++];
-                if (type == INT_FIXED_LEN || type == BYTESTRING_FIXED_LEN)
+                if (type == UINT_FIXED_LEN || type == BYTESTRING_FIXED_LEN)
                     writeContent += EncodeSingle(row[rowIndexer++], type, columnTypes[columnTypesIndexer++]);
                 else
                     writeContent += EncodeSingle(row[rowIndexer++], type, 0);
@@ -187,6 +190,30 @@ namespace SecondaryIndex
             new StorageMap(ROWS_PREFIX).Delete(user + tableName + SEPARATOR + primaryKey);
         }
 
+        public static (object, byte[]) DecodeSingle(byte[] encoded, byte type, byte length)
+        {
+            switch (type)
+            {
+                case BOOLEAN:
+                    return (encoded[0] > 0 ? true : false, encoded[1..]);
+                case UINT160:
+                    return (encoded[..20], encoded[20..]);
+                case UINT256:
+                    return (encoded[..32], encoded[32..]);
+                case INT_VAR_LEN:
+                    return DecodeInteger(encoded);
+                case BYTESTRING_VAR_LEN:
+                    return DecodeByteString(encoded);
+                case UINT_FIXED_LEN:
+                    return DecodeIntegerFixedLength(encoded, length);
+                case BYTESTRING_FIXED_LEN:
+                    return DecodeByteStringFixedLength(encoded, length);
+                default:
+                    throw new ArgumentException("Unsupported type " + type);
+            }
+
+        }
+
         /// <summary>
         /// 
         /// </summary>
@@ -195,24 +222,44 @@ namespace SecondaryIndex
         /// <param name="primaryKey">rowId or content of 1st column</param>
         /// <returns></returns>
         /// <exception cref="ArgumentException"></exception>
-        public static object[] GetRow(UInt160 user, ByteString tableName, ByteString primaryKey)
+        public static object[] GetRow(UInt160 user, ByteString tableName, object primaryKey)
         {
             StorageContext context = Storage.CurrentContext;
             ByteString key = user + tableName;
             ByteString columnTypes = new StorageMap(context, USER_TABLE_NAME_TO_COLUMNS_PREFIX)[key];
+            if (columnTypes == null)
+                throw new ArgumentException("No table");
+            ByteString rowId = new StorageMap(context, TABLE_ROW_ID_PREFIX)[key];
             BigInteger columnTypesLength = columnTypes.Length;
-            byte[] data = (byte[])new StorageMap(context, ROWS_PREFIX)[key + SEPARATOR + primaryKey];
+            byte[] data = (byte[])new StorageMap(context, ROWS_PREFIX)[key + SEPARATOR + (ByteString)primaryKey];
             if (data == null)
                 throw new ArgumentException("No data");
             List<object> row = new();
-            for (int columnTypesIndexer = 0; columnTypesIndexer < columnTypesLength; columnTypesIndexer++)
+            int columnTypesIndexer = 0;
+            if (rowId == null)
             {
-                byte type = columnTypes[columnTypesIndexer];
+                // skip the first column of columnTypes, because it is the primary key
+                byte primaryKeyType = columnTypes[columnTypesIndexer++];
+                if (primaryKeyType == UINT_FIXED_LEN || primaryKeyType == BYTESTRING_FIXED_LEN)
+                    columnTypesIndexer++;
+                row.Add(primaryKey);
+            }
+            while (columnTypesIndexer < columnTypesLength)
+            {
+                byte type = columnTypes[columnTypesIndexer++];
                 switch (type)
                 {
                     case BOOLEAN:
                         row.Add(data[0] > 0 ? true : false);
                         data = data[1..];
+                        break;
+                    case UINT160:
+                        row.Add(data[..20]);
+                        data = data[20..];
+                        break;
+                    case UINT256:
+                        row.Add(data[..32]);
+                        data = data[32..];
                         break;
                     case INT_VAR_LEN:
                         (BigInteger i, data) = DecodeInteger(data);
@@ -222,12 +269,12 @@ namespace SecondaryIndex
                         (ByteString s, data) = DecodeByteString(data);
                         row.Add(s);
                         break;
-                    case INT_FIXED_LEN:
-                        (BigInteger iFixed, data) = DecodeBigIntegerFixedLength(data, columnTypes[++columnTypesIndexer]);
+                    case UINT_FIXED_LEN:
+                        (BigInteger iFixed, data) = DecodeIntegerFixedLength(data, columnTypes[columnTypesIndexer++]);
                         row.Add(iFixed);
                         break;
                     case BYTESTRING_FIXED_LEN:
-                        (ByteString sFixed, data) = DecodeByteStringFixedLength(data, columnTypes[++columnTypesIndexer]);
+                        (ByteString sFixed, data) = DecodeByteStringFixedLength(data, columnTypes[columnTypesIndexer++]);
                         row.Add(sFixed);
                         break;
                     default:
@@ -294,7 +341,7 @@ namespace SecondaryIndex
             encoded = encoded[fixedLength..];
             return (d, encoded);
         }
-        public static (BigInteger, byte[]) DecodeBigIntegerFixedLength(byte[] encoded, int fixedLength)
+        public static (BigInteger, byte[]) DecodeIntegerFixedLength(byte[] encoded, int fixedLength)
         {
             (ByteString d, encoded) = DecodeByteStringFixedLength(encoded, fixedLength);
             return ((BigInteger)d, encoded);
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-from neo_fairy_client import FairyClient, Hash160Str
+from neo_fairy_client import FairyClient, Hash160Str, Hash256Str
 from enum import Enum
 
 user = Hash160Str('0xb1983fa2479a0c8e2beae032d2df564b5451b7a5')
@@ -8,13 +8,18 @@ print(result := c.invokefunction('encodeInteger', [12345678]))
 assert c.invokefunction('decodeInteger', [result]) == (12345678, '')
 print(result := c.invokefunction('encodeInteger', [-12345678]))
 assert c.invokefunction('decodeInteger', [result]) == (-12345678, '')
+assert len(c.invokefunction('encodeInteger', [-1])) == 9
+assert len(c.invokefunction('encodeInteger', [-2**33])) == 13
+assert c.invokefunction('decodeIntegerFixedLength', [c.invokefunction('encodeIntegerFixedLength', [-1, 4]), 4]) == (255, '')
 
 
 class Types(bytes, Enum):
+    UINT160 = b'\x10'
+    UINT256 = b'\x11'
     Boolean = b'\x20'
     IntegerVarLen = b'\x21'
     ByteStringVarLen = b'\x28'
-    IntegerFixedLen = b'\x31'
+    UnsignedIntegerFixedLen = b'\x31'
     ByteStringFixedLen = b'\x38'
     
     def __add__(self, other):
@@ -23,11 +28,11 @@ class Types(bytes, Enum):
 
 table_name = 'testTable'
 assert 'Invalid type' in c.invokefunction('createTable', [user, table_name, Types.Boolean + Types.IntegerVarLen + b'\x00', True], do_not_raise_on_result=True)
-assert 'SEPARATOR in tableName' in c.invokefunction('createTable', [user, 'testTable¶', Types.Boolean + Types.IntegerVarLen + Types.ByteStringVarLen + Types.IntegerFixedLen + b'\x04' + Types.ByteStringFixedLen + b'\x20', True], do_not_raise_on_result=True)
+assert 'SEPARATOR in tableName' in c.invokefunction('createTable', [user, 'testTable¶', Types.Boolean + Types.IntegerVarLen + Types.ByteStringVarLen + Types.UnsignedIntegerFixedLen + b'\x04' + Types.ByteStringFixedLen + b'\x20', True], do_not_raise_on_result=True)
 c.invokefunction(
     'createTable', [
         user, 'testTable',
-        Types.Boolean + Types.IntegerVarLen + Types.ByteStringVarLen + Types.IntegerFixedLen + b'\x04' + Types.ByteStringFixedLen + b'\x20',
+        Types.Boolean + Types.IntegerVarLen + Types.ByteStringVarLen + Types.UnsignedIntegerFixedLen + b'\x04' + Types.ByteStringFixedLen + b'\x20',
         # bool, int, str, int32, str(20)
         True
     ])
@@ -50,7 +55,27 @@ c.invokefunction('deleteRow', [user, table_name, 2])
 c.invokefunction('deleteRow', [user, table_name, 0])  # no id 0, but should not raise error
 
 c.invokefunction('dropTable', [user, table_name])
+assert 'No table' in c.invokefunction('getRow', [user, table_name, 1], do_not_raise_on_result=True)
 assert len(c.invokefunction('listTables', [user, ''])) == 0
 assert len(c.invokefunction('listAllTables')) == 0
 assert len(c.invokefunction('listDroppedTables', [user, ''])) == 1
 assert len(c.invokefunction('listAllDroppedTables')) == 1
+
+# be aware that Neo3 allows only storage key <= 64 bytes
+table_name = 'customPrimaryKey'
+c.invokefunction(
+    'createTable', [
+        user, table_name,
+        Types.UINT160 + Types.UINT256 + Types.UnsignedIntegerFixedLen + b'\x04',
+        False
+    ])
+data = [
+    [Hash160Str('0x'+'00'*20), Hash256Str('0x'+'00'*32), 1],
+    [Hash160Str('0x'+'00'*19+'01'), Hash256Str('0x'+'00'*31+'01'), -2],
+    [Hash160Str('0x'+'00'*19+'02'), Hash256Str('0x'+'00'*31+'02'), -3],
+]
+for row in data:
+    c.invokefunction('writeRow', [user, table_name, row])
+assert c.invokefunction('getRow', [user, table_name, data[0][0]]) == ['\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', '\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', 1]
+assert c.invokefunction('getRow', [user, table_name, data[1][0]]) == ['\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', '\x01\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', 254]
+assert c.invokefunction('getRow', [user, table_name, data[2][0]]) == ['\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', '\x02\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00', 253]
